be8b1c6b9febbee3b880808a126cf689fb79b8cc
summerscar <shen.yus@163.com>
2017-08-29 12:36:30 +0800

    fix

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
 const moment = require('moment')
 
-module.exports = {
-    ak: '786f78a1ea2f43beba7ba2b68949ae34',
-    memobirdID: '290c93e55b890d8a',
-    useridentifying: '929019',
-    timestamp: moment().format('YYYY-MM-DD HH:mm:ss')
-}
+module.exports = {}
--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,10 @@ router.get('/', async (ctx, next) => {
 })
 
 router.post('/printText', async (ctx, next) => {
-  const memobird = new Memobird({ak: '786f78a1ea2f43beba7ba2b68949ae34',memobirdID: '290c93e55b890d8a',useridentifying: '929019'})
+  const memobird = new Memobird({ak: '',memobirdID: '',useridentifying: ''})
   ctx.body = {code:1}
   memobird.init()
   .then(() => memobird.printText(ctx.request.body.content))
-  //.then( res => memobird.printImg('http://wx4.sinaimg.cn/mw690/006Flhywly1fizklxylh3j30gs0e3n52.jpg'))
   .then( res => memobird.status(res.printcontentid, 3000))
   .then(printflag => {
     console.log('检测完成',printflag === 1 ? '打印完成' : '打印未完成')
@@ -24,7 +23,7 @@ router.post('/printText', async (ctx, next) => {
 })
 
 router.post('/printImg', async (ctx, next) => {
-  const memobird = new Memobird({ak: '786f78a1ea2f43beba7ba2b68949ae34',memobirdID: '290c93e55b890d8a',useridentifying: '929019'})
+  const memobird = new Memobird({ak: '',memobirdID: '',useridentifying: ''})
   ctx.body = {code:1}
   memobird.init() 
   .then( res => memobird.printImg(ctx.request.body.content))
