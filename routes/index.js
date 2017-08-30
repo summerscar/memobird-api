0bcb284920ab1654c6901caa8c34c46919b6cdc4
summerscar <shen.yus@163.com>
2017-08-30 13:14:43 +0800

    删除信息

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', async (ctx, next) => {
 })
 
 router.post('/printText', async (ctx, next) => {
-  const memobird = new Memobird({ak: '786f78a1ea2f43beba7ba2b68949ae34',memobirdID: '290c93e55b890d8a',useridentifying: '929019'})
+  const memobird = new Memobird({ak: '',memobirdID: '',useridentifying: ''})
   ctx.body = {code:1}
   memobird.init()
   .then(() => memobird.printText(ctx.request.body.content))
@@ -23,7 +23,7 @@ router.post('/printText', async (ctx, next) => {
 })
 
 router.post('/printImg', async (ctx, next) => {
-  const memobird = new Memobird({ak: '786f78a1ea2f43beba7ba2b68949ae34',memobirdID: '290c93e55b890d8a',useridentifying: '929019'})
+  const memobird = new Memobird({ak: '',memobirdID: '',useridentifying: ''})
   ctx.body = {code:1}
   memobird.init() 
   .then( res => memobird.printImg(ctx.request.body.content))
@@ -38,7 +38,7 @@ router.post('/printImg', async (ctx, next) => {
 })
 
 router.post('/printMutilContent', async (ctx, next) => {
-  const memobird = new Memobird({ak: '786f78a1ea2f43beba7ba2b68949ae34',memobirdID: '290c93e55b890d8a',useridentifying: '929019'})
+  const memobird = new Memobird({ak: '',memobirdID: '',useridentifying: ''})
   memobird.init()
   .then(() => memobird.printMutilContent(ctx.request.body.content))
   .then( res => memobird.status(res.printcontentid, 3000))
