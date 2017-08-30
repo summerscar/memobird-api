8022b4586a8c9a7ffd6cb805769b8243c4c67960
summerscar <shen.yus@163.com>
2017-08-30 14:50:20 +0800

    fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 
 module.exports = {
-    ak: '786f78a1ea2f43beba7ba2b68949ae34',
-    memobirdID: '290c93e55b890d8a',
-    useridentifying: '929019'
+    ak: '',
+    memobirdID: '',
+    useridentifying: ''
 }
